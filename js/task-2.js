8b424aca6beefcf0e611453db23947b19eed38bc
Zhanna Slovak <zhannashost@gmail.com>
2026-03-06 13:57:42 +0200

    add js-hw-03

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    const arr = firstArray.concat(secondArray);
+    if (arr.length > maxLength) {
+        return arr.slice(0, maxLength);
+    } else { return arr; }
+}
+
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
